d82ab1d288271b2f11d682894dce39931494cf9c
Сандро <warsan00@mail.ru>
2020-09-28 04:39:37 +0500

    Правки

---
+++ src/contributing/link-target.md
@@ -0,0 +1,1 @@
+!button[Return](./button-links)
--- src/usage-examples/precompilation/precompile-in-nodejs.js
+++ src/usage-examples/precompilation/precompile-in-nodejs.js
@@ -1,4 +1,4 @@
-let template = "Handlebars <b>{{doesWhat}}</b> предварительно скомпилирован!";
+let template = "Handlebars <b>{{doesWhat}}</b> предскомпилирован!";
 let Handlebars = require("handlebars");
 let compiled = Handlebars.precompile(template);
 console.log(compiled);
